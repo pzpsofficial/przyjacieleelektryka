3315f976c2df291d31bc6708928a12cdd35511d5
pzpsofficial <53826083+pzpsofficial@users.noreply.github.com>
2021-01-22 19:47:46 +0100

    Build hero section

---
+++ scripts/type.js
@@ -0,0 +1,12 @@
+import Typed from 'typed.js';
+
+let options = {
+  strings: ['Pszczoły', 'Młodzież', 'Talenty', 'Przyrodę', 'Potrzebujących'],
+  typeSpeed: 150,
+  backSpeed: 50,
+  startDelay: 1000,
+  backDelay: 1000,
+  loop: true,
+};
+
+let typed = new Typed('.typed', options);
