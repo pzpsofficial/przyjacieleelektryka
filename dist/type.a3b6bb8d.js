f17f249a1be93a6d2522ba6089e0f2d6d1312878
pzpsofficial <53826083+pzpsofficial@users.noreply.github.com>
2021-01-26 22:36:29 +0100

    Created frontside of slider

--- dist/type.a3b6bb8d.js
+++ dist/type.a3b6bb8d.js
@@ -1179,12 +1179,13 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var options = {
   strings: ['Pszczoły', 'Młodzież', 'Talenty', 'Przyrodę', 'Potrzebujących'],
-  typeSpeed: 150,
+  typeSpeed: 100,
   backSpeed: 50,
   smartBackspace: false,
   startDelay: 1000,
   backDelay: 1000,
-  loop: true
+  loop: true,
+  shuffle: true
 };
 var typed = new _typed.default('.typed', options);
 },{"typed.js":"node_modules/typed.js/lib/typed.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
@@ -1215,7 +1216,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51745" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57702" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- scripts/type.js
+++ scripts/type.js
@@ -2,12 +2,13 @@ import Typed from 'typed.js';
 
 let options = {
   strings: ['Pszczoły', 'Młodzież', 'Talenty', 'Przyrodę', 'Potrzebujących'],
-  typeSpeed: 150,
+  typeSpeed: 100,
   backSpeed: 50,
   smartBackspace: false,
   startDelay: 1000,
   backDelay: 1000,
   loop: true,
+  shuffle: true,
 };
 
 let typed = new Typed('.typed', options);
